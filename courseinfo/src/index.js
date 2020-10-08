e0722b0f6e9218442d7c17c7b527effb2b7a7b9c
Cormac Lawlor <armedcor@gmail.com>
2020-10-08 21:50:15 +0000

    refactor App into 3 sub components

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = (prop) => {
+    
+
+    return (
+        <>
+        <h1>{prop.course}</h1>
+        </>
+    )
+}
+
+const Content = (prop) => (
+    <>
+    <p>
+        {prop.part1} {prop.exercise1}
+    </p>
+    <p>
+        {prop.part2} {prop.exercise2}
+    </p>
+    <p>
+        {prop.part3} {prop.exercise3}
+    </p>
+    </>
+)
+
+const Total = (prop) => (
+    <>
+    <p>Number of exercises {prop.exercise1 + prop.exercise2 + prop.exercise3}</p>
+    </>
+)
 
 const App = () => {
     const course = "Half Stack application development";
@@ -13,17 +42,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercise1}
-            </p>
-            <p>
-                {part2} {exercise2}
-            </p>
-            <p>
-                {part3} {exercise3}
-            </p>
-            <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
+            <Header course={course} />
+            <Content part1={part1} exercise1={exercise1} part2={part2} exercise2={exercise2} part3={part3} exercise3={exercise3} />
+            <Total exercise1={exercise1} exercise2={exercise2} exercise3={exercise3} />
         </div>
     )
 }
