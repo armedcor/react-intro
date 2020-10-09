582b5c2431b41471d422e4fdc6254c548dc28954
Cormac Lawlor <armedcor@gmail.com>
2020-10-09 20:38:45 +0000

    Quick a dirty(bad) refactoring

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -11,19 +11,28 @@ const Header = (prop) => {
     )
 }
 
-const Content = (prop) => (
-    <>
+const Part = (prop) => (
     <p>
-        {prop.part1} {prop.exercise1}
+        {prop.part} {prop.exercise}
     </p>
-    <p>
-        {prop.part2} {prop.exercise2}
-    </p>
-    <p>
-        {prop.part3} {prop.exercise3}
-    </p>
-    </>
 )
+
+const Content = () => {
+    const part1 = "Fundamentals of React";
+    const exercise1 = 10;
+    const part2 = "Using props to pass data";
+    const exercise2 = 7;
+    const part3 = "State of a component";
+    const exercise3 = 14;
+    return (
+        <div>
+            <Part part={part1} exercise={exercise1} />
+            <Part part={part2} exercise={exercise2} />
+            <Part part={part3} exercise={exercise3} />
+        </div>
+    )
+}
+
 
 const Total = (prop) => (
     <>
@@ -33,17 +42,14 @@ const Total = (prop) => (
 
 const App = () => {
     const course = "Half Stack application development";
-    const part1 = "Fundamentals of React";
     const exercise1 = 10;
-    const part2 = "Using props to pass data";
     const exercise2 = 7;
-    const part3 = "State of a component";
     const exercise3 = 14;
 
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1} exercise1={exercise1} part2={part2} exercise2={exercise2} part3={part3} exercise3={exercise3} />
+            <Content />
             <Total exercise1={exercise1} exercise2={exercise2} exercise3={exercise3} />
         </div>
     )
